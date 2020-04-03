d771875a85b70fbc750a3987f5ef275af5a40fb0
Aniket Raj <aniketronaldo10@gmail.com>
2020-04-03 16:26:12 +0530

    Check why the fuck is the form getting submitted even when I add code to not

--- js/battleofvars.js
+++ js/battleofvars.js
@@ -1,4 +1,4 @@
-function validateInput() {
+function validateName() {
     let name = $("#name").val();
     let letters = /^[A-Za-z ]+$/;
     if (!(name.match(letters))) {
@@ -7,12 +7,49 @@ function validateInput() {
     }
     return true;
 }
-$("form").submit(function (event) {
-    if (validateInput()) {
-        alert("TRUE")
-        return;
+
+function validatePhone() {
+    let phone = $("#phoneNum").val();
+    let phoneNum = phone.replace(/[^\d]/g, '');
+    if (phoneNum.length == 10) { return true; }
+    $("#phoneNum").css("border", "4px solid red");
+    return false;
+}
+
+function validateEmail() {
+    let mail = $("#email").val();
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        return true;
     }
-    alert("false")
-    $("#name").css("border", "4px solid red");
-    event.preventDefault();
-});
+    $("#email").css("border", "4px solid red");
+    return false;
+}
+
+$("#form-new").submit(function (e) {
+    e.preventDefault();
+    let flg = 1;
+    alert('9isaudhfisauhd')
+    if (!validateName()) { flg = 0; }
+    if (!validatePhone()) { flg = 0; }
+    if (!validateEmail()) { flg = 0; }
+    if (flg) {
+        data = {
+            'name': $("#name").val(),
+            'email': $("#email").val(),
+            'phone': $("#phoneNum").val(),
+            'hackerrank_username': $("#hacker_id").val(),
+            'country': $("#country").val(),
+            'email_content': $("#email_content").val(),
+            'event': $("#event").val(),
+            'db': $("#db").val(),
+            'date': $("#date").val(),
+            'no_qr': $("#no_qr").val(),
+        }
+        console.log(data)
+    } else {
+        alert("Please check the highlighted inputs")
+        return false;
+    }
+ }); 
+
+
