9ab3efb16e51a6a4bddc043c98c9c954bf706b85
PranavBakre <psbakre123@gmail.com>
2020-04-19 22:54:47 +0530

    replaced option fields for faculty with script

--- js/Coursera.js
+++ js/Coursera.js
@@ -1,26 +1,30 @@
+$(document).ready(() => {
+    $.getJSON("js/faculties.json", data => {
+        data.forEach(element => {
+            $("#faculty").append(`<option value='${element}'>${element}</option>`);
+        })
+    });
+});
+
 function getSchools() {
-    
-    $.getJSON("js/schools.json",data=> {
-        
-        
-        let faculty=$("#faculty").val();
-        console.log(data[faculty])
+
+    $.getJSON("js/schools.json", data => {
+        let faculty = $("#faculty").val();
         $("#school").html("");
-        data[faculty].forEach((element,index) => {
+        data[faculty].forEach((element, index) => {
             $("#school").append(`<option value="${element[0]}"  >${element[1]}</option>`);
         });
     });
     getPrograms();
 
     $('.field').removeClass("d-none");
-    
-   
+
+
 }
 
-function getPrograms(){
+function getPrograms() {
     $.getJSON("js/programs.json", data => {
-        console.log(data)
-        let school= $("#school").val();
+        let school = $("#school").val();
         $("#program").html("");
         data[school].forEach(element => {
             $("#program").append(`<option value="${element[0]}" >${element[1]}</option>`);
