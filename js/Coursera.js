50c6b68a1464311b69b2d451eeb44c728f2a1192
PranavBakre <psbakre123@gmail.com>
2020-04-19 19:59:00 +0530

    Kuch to hai XD

--- js/Coursera.js
+++ js/Coursera.js
@@ -1,9 +1,29 @@
-function setPrograms() {
+function getSchools() {
+    
+    $.getJSON("js/schools.json",data=> {
+        
+        
+        let faculty=$("#faculty").val();
+        console.log(data[faculty])
+        $("#school").html("");
+        data[faculty].forEach((element,index) => {
+            $("#school").append(`<option value="${element[0]}"  >${element[1]}</option>`);
+        });
+    });
+    getPrograms();
+
+    $('.field').removeClass("d-none");
+    
+   
+}
+
+function getPrograms(){
     $.getJSON("js/programs.json", data => {
+        console.log(data)
         let school= $("#school").val();
         $("#program").html("");
         data[school].forEach(element => {
-            $("#program").append(`<option value="${element[0]}">${element[1]}</option>`);
+            $("#program").append(`<option value="${element[0]}" >${element[1]}</option>`);
         });
     })
 }
