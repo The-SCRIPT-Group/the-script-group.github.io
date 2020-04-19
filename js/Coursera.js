994d1855766161779566eb6875de3a1fcce1dc49
ksdfg <ksdfg123@gmail.com>
2020-04-20 02:15:43 +0530

    material css ftw

--- js/Coursera.js
+++ js/Coursera.js
@@ -1,9 +1,30 @@
+facultySelect = true;
+
 $(document).ready(() => {
+    $('select').on('contentChanged', function () {
+        $(this).formSelect();
+        $('.select-dropdown').addClass('white-text');
+        $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
+    });
+
+    $("#faculty").on('change', () => {
+        getSchools();
+    })
+
     $.getJSON("js/faculties.json", data => {
         data.forEach(element => {
             $("#faculty").append(`<option value='${element}'>${element}</option>`);
-        })
+        });
+        $('#faculty').trigger('contentChanged');
     });
+
+    $("#reg-form").on('submit', (e) => {
+        validate();
+    })
+
+    $('select').formSelect();
+    $('.select-dropdown').addClass('white-text');
+    $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
 });
 
 function getSchools() {
@@ -14,10 +35,13 @@ function getSchools() {
         data[faculty].forEach((element, index) => {
             $("#school").append(`<option value="${element[0]}"  >${element[1]}</option>`);
         });
+
+        $('#school').trigger('contentChanged');
     });
+
     getPrograms();
 
-    $('.field').removeClass("d-none");
+    $('.field').css("display", "block");
 
 
 }
@@ -29,5 +53,17 @@ function getPrograms() {
         data[school].forEach(element => {
             $("#program").append(`<option value="${element[0]}" >${element[1]}</option>`);
         });
+
+        $('#program').trigger('contentChanged');
     })
+}
+
+function validate(e) {
+    console.log($("#school").val());
+    if ($("#faculty").val() == "") {
+        e.preventDefault();
+        alert("Please select Faculty");
+        return false;
+    }
+    return false;
 }
