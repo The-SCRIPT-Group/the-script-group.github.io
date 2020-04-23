f258ef3ae49b9fc37695ea15165fceb2f9d4748a
Akhil Narang <me@akhilnarang.dev>
2020-04-23 16:51:55 +0530

    coursera: formatting
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- js/Coursera.js
+++ js/Coursera.js
@@ -1,50 +1,50 @@
 $(document).ready(() => {
-    $('select').on('contentChanged', function () {
-        $(this).formSelect();
-        $('.select-dropdown').addClass('white-text');
-        $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
-    });
-    $('#modal').modal();
-
-    $("#reg-form").on("reset", () => {
-        $(".clogo").slideDown();
-        $(".field").slideUp();
-        $(".f2").slideUp();
-    })
-
-    $("#faculty").on('change', () => {
-        if ($("#faculty").val() !== "")
-            getSchools();
-    })
-
-    $("#school").on('change', () => {
-        getPrograms();
-    })
-
-    $("#name,#prn").on('focus', () => {
-        $(".clogo").slideUp();
-        $(".f2").slideDown();
-
-    });
-
-    $.getJSON("js/faculties.json", data => {
-        data.forEach(element => {
-            $("#faculty").append(`<option value='${element}'>${element}</option>`);
-        });
-
-        $('#faculty').trigger('contentChanged');
-    });
-
-    $('select').formSelect();
+  $('select').on('contentChanged', function () {
+    $(this).formSelect();
     $('.select-dropdown').addClass('white-text');
     $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
+  });
+  $('#modal').modal();
 
-    $("#reg-form").on('submit', e => {
-        e.preventDefault();
-        let validflag = true;
-        validflag = validate();
-        if (validflag) {
-        let email_content = `
+  $("#reg-form").on("reset", () => {
+    $(".clogo").slideDown();
+    $(".field").slideUp();
+    $(".f2").slideUp();
+  })
+
+  $("#faculty").on('change', () => {
+    if ($("#faculty").val() !== "")
+      getSchools();
+  })
+
+  $("#school").on('change', () => {
+    getPrograms();
+  })
+
+  $("#name,#prn").on('focus', () => {
+    $(".clogo").slideUp();
+    $(".f2").slideDown();
+
+  });
+
+  $.getJSON("js/faculties.json", data => {
+    data.forEach(element => {
+      $("#faculty").append(`<option value='${element}'>${element}</option>`);
+    });
+
+    $('#faculty').trigger('contentChanged');
+  });
+
+  $('select').formSelect();
+  $('.select-dropdown').addClass('white-text');
+  $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
+
+  $("#reg-form").on('submit', e => {
+    e.preventDefault();
+    let validflag = true;
+    validflag = validate();
+    if (validflag) {
+      let email_content = `
         <!doctype html>
 <html>
   <head>
@@ -141,7 +141,7 @@ $(document).ready(() => {
     }
     </style>
   </head>`
-  let email_formattable_content=`
+      let email_formattable_content = `
   <body class="" style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
     <table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;">
       <tr>
@@ -229,94 +229,91 @@ $(document).ready(() => {
   </body>
 </html>
 `
-data = {
-    'name': $("#name").val(),
-    'email': $("#email").val(),
-    'phone': $("#phno").val(),
-    'prn': $("#prn").val(),
-    'faculty': $("#faculty").val(),
-    'school': $("#school").val(),
-    'program': $("#program").val(),
-    'year': $("#year").val(),
-    'email_content': emailContent,
-    'email_formattable_content': email_formattable_content,
-    'email_fields': 'name,email',
-    'no_qr': '',
-}
+      data = {
+        'name': $("#name").val(),
+        'email': $("#email").val(),
+        'phone': $("#phno").val(),
+        'prn': $("#prn").val(),
+        'faculty': $("#faculty").val(),
+        'school': $("#school").val(),
+        'program': $("#program").val(),
+        'year': $("#year").val(),
+        'email_content': emailContent,
+        'email_formattable_content': email_formattable_content,
+        'email_fields': 'name,email',
+        'no_qr': '',
+      }
 
+      $(".main-loader").css("display", "flex")
+      $.post("https://hades.thescriptgroup.in/submit",
+        data,
+        (data, status) => {
+          $(".main-loader").css("display", "none")
+          var instance = M.Modal.getInstance($("#modal"));
+          $(".modal-content").html(data);
 
-
-
-            $(".main-loader").css("display", "flex")
-            $.post("https://hades.thescriptgroup.in/submit",
-                data,
-                (data, status) => {
-                    $(".main-loader").css("display", "none")
-                    var instance = M.Modal.getInstance($("#modal"));
-                    $(".modal-content").html(data);
-
-                    instance.open();
-                    $("#reg-form").trigger("reset");
-                }
-            )
+          instance.open();
+          $("#reg-form").trigger("reset");
         }
-    })
+      )
+    }
+  })
 });
 
 function getSchools() {
 
-    $.getJSON("js/schools.json", data => {
-        let faculty = $("#faculty").val();
-        $("#school").html("");
-        data[faculty].forEach((element, index) => {
-            $("#school").append(`<option value="${element[0]}"  >${element[1]}</option>`);
-        });
-
-        $('#school').trigger('contentChanged');
+  $.getJSON("js/schools.json", data => {
+    let faculty = $("#faculty").val();
+    $("#school").html("");
+    data[faculty].forEach((element, index) => {
+      $("#school").append(`<option value="${element[0]}"  >${element[1]}</option>`);
     });
 
-    getPrograms();
+    $('#school').trigger('contentChanged');
+  });
 
-    $('.field').slideDown();
+  getPrograms();
+
+  $('.field').slideDown();
 
 }
 
 function getPrograms() {
-    $.getJSON("js/programs.json", data => {
-        let school = $("#school").val();
-        $("#program").html("");
-        data[school].forEach(element => {
-            $("#program").append(`<option value="${element[0]}" >${element[1]}</option>`);
-        });
+  $.getJSON("js/programs.json", data => {
+    let school = $("#school").val();
+    $("#program").html("");
+    data[school].forEach(element => {
+      $("#program").append(`<option value="${element[0]}" >${element[1]}</option>`);
+    });
 
-        $('#program').trigger('contentChanged');
-    })
+    $('#program').trigger('contentChanged');
+  })
 }
 
 function validate() {
-    var res = true;
+  var res = true;
 
-    if ($("#name").val().split(" ").length <= 1) {
-        M.toast({ html: "Please enter full name" });
-        res = false;
-    }
+  if ($("#name").val().split(" ").length <= 1) {
+    M.toast({ html: "Please enter full name" });
+    res = false;
+  }
 
-    if ($("#faculty").val() == "") {
-        M.toast({ html: "Please select Faculty" });
-        res = false;
-    }
+  if ($("#faculty").val() == "") {
+    M.toast({ html: "Please select Faculty" });
+    res = false;
+  }
 
-    let phone = $("#phno").val();
-    if (phone.length != 10 || phone.match(/^\d+$/) == null) {
-        M.toast({ html: "Please enter the correct phone number" });
-        res = false;
-    }
+  let phone = $("#phno").val();
+  if (phone.length != 10 || phone.match(/^\d+$/) == null) {
+    M.toast({ html: "Please enter the correct phone number" });
+    res = false;
+  }
 
-    let prn = $("#prn").val()
-    if (prn.length != 10 || prn.match(/^\d{10}$/) == null) {
-        M.toast({ html: "Please enter the correct PRN! Remember, your PRN consists only of numbers." });
-        res = false;
-    }
+  let prn = $("#prn").val()
+  if (prn.length != 10 || prn.match(/^\d{10}$/) == null) {
+    M.toast({ html: "Please enter the correct PRN! Remember, your PRN consists only of numbers." });
+    res = false;
+  }
 
-    return res;
+  return res;
 }
