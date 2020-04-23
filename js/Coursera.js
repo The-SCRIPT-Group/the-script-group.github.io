eb1e68ee7e8a7811154a3062c49ba3d460f85538
Akhil Narang <me@akhilnarang.dev>
2020-04-23 16:23:28 +0530

    coursera: formatting, remove some newlines
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- js/Coursera.js
+++ js/Coursera.js
@@ -5,15 +5,15 @@ $(document).ready(() => {
         $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
     });
     $('#modal').modal();
-    
-    $("#reg-form").on("reset", () =>{
+
+    $("#reg-form").on("reset", () => {
         $(".clogo").slideDown();
         $(".field").slideUp();
         $(".f2").slideUp();
     })
 
     $("#faculty").on('change', () => {
-        if ($("#faculty").val()!=="")
+        if ($("#faculty").val() !== "")
             getSchools();
     })
 
@@ -24,8 +24,9 @@ $(document).ready(() => {
     $("#name,#prn").on('focus', () => {
         $(".clogo").slideUp();
         $(".f2").slideDown();
-        
+
     });
+
     $.getJSON("js/faculties.json", data => {
         data.forEach(element => {
             $("#faculty").append(`<option value='${element}'>${element}</option>`);
@@ -34,49 +35,45 @@ $(document).ready(() => {
         $('#faculty').trigger('contentChanged');
     });
 
-
-
     $('select').formSelect();
     $('.select-dropdown').addClass('white-text');
     $('.dropdown-content').css('background', 'rgba(25, 28, 29, 0.95)');
 
+    $("#reg-form").on('submit', e => {
+        e.preventDefault();
+        let validflag = true;
+        validflag = validate();
 
-$("#reg-form").on('submit',e => {
-    e.preventDefault();
-    let validflag=true;
-    validflag=validate();
+        let emailContent = `PratikIsGay`;
 
-    let emailContent=`PratikIsGay`;
-    
-
-    if (validflag) {
-        data= {
-            'name':$("#name").val(),
-            'email':$("#email").val(),
-            'phone':$("#phno").val(),
-            'prn':$("#prn").val(),
-            'faculty':$("#faculty").val(),
-            'school':$("#school").val(),
-            'program':$("#program").val(),
-            'year':$("#year").val(),
-            'email_content':emailContent,
-            'no_qr': '',       
-        }
-        
-        $(".main-loader").css("display","flex")
-        $.post("https://hades.thescriptgroup.in/submit",
-            data,
-            (data,status) => {
-                $(".main-loader").css("display","none")
-                var instance = M.Modal.getInstance($("#modal"));
-                $(".modal-content").html(data);
-        
-                instance.open();
-                $("#reg-form").trigger("reset");
+        if (validflag) {
+            data = {
+                'name': $("#name").val(),
+                'email': $("#email").val(),
+                'phone': $("#phno").val(),
+                'prn': $("#prn").val(),
+                'faculty': $("#faculty").val(),
+                'school': $("#school").val(),
+                'program': $("#program").val(),
+                'year': $("#year").val(),
+                'email_content': emailContent,
+                'no_qr': '',
             }
-        )
-    }
-})
+
+            $(".main-loader").css("display", "flex")
+            $.post("https://hades.thescriptgroup.in/submit",
+                data,
+                (data, status) => {
+                    $(".main-loader").css("display", "none")
+                    var instance = M.Modal.getInstance($("#modal"));
+                    $(".modal-content").html(data);
+
+                    instance.open();
+                    $("#reg-form").trigger("reset");
+                }
+            )
+        }
+    })
 });
 
 function getSchools() {
@@ -94,7 +91,6 @@ function getSchools() {
     getPrograms();
 
     $('.field').slideDown();
-
 
 }
 
@@ -122,17 +118,17 @@ function validate() {
         M.toast({ html: "Please select Faculty" });
         res = false;
     }
-    
-    let phone=$("#phno").val();
-    if (phone.length != 10 || phone.match(/^\d+$/)==null) {
-        M.toast({html:"Please enter the correct phone number"});
-        res=false;
+
+    let phone = $("#phno").val();
+    if (phone.length != 10 || phone.match(/^\d+$/) == null) {
+        M.toast({ html: "Please enter the correct phone number" });
+        res = false;
     }
-    
-    let prn= $("#prn").val()
-    if (prn.length != 10 || prn.match(/^\d{10}$/)==null) {
-        M.toast({html:"Please enter the correct PRN! Remember, your PRN consists only of numbers."});
-        res=false;
+
+    let prn = $("#prn").val()
+    if (prn.length != 10 || prn.match(/^\d{10}$/) == null) {
+        M.toast({ html: "Please enter the correct PRN! Remember, your PRN consists only of numbers." });
+        res = false;
     }
 
     return res;
