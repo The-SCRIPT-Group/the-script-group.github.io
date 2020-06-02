58cdb82983857c9ae2e2a32d5e3414c1c77915e7
Aniket Raj <aniketronaldo10@gmail.com>
2020-06-02 10:33:39 +0530

    added img path field

--- js/members.js
+++ js/members.js
@@ -1,6 +1,7 @@
 const members = [
     {
         'name': 'Aniket Raj',
+        'img': 'images/Aniket.png',
         'title': 'President',
         'desc': `Winner of Ready Set Code '18, and our primary Techo Lecturer, Aniket
         brings out the best of the team with his wide-ranging skills, but is still immature with
@@ -14,6 +15,7 @@ const members = [
     },
     {
         'name': 'Ritom Gupta',
+        'img': 'images/Ritom.png',
         'title': 'Chairman & Co-founder',
         'desc': `The crazy, yet sensible founder of S.C.R.I.P.T, Ritom manages the team in
         all activities, and
@@ -27,6 +29,7 @@ const members = [
     },
     {
         'name': 'Vignesh Iyer',
+        'img': 'images/Vignesh.PNG',
         'title': 'Co-Founder & Joint Treasurer',
         'desc': `The silent and dependable founder of S.C.R.I.P.T, Vignesh is our finance
         head and chief
@@ -40,6 +43,7 @@ const members = [
     },
     {
         'name': 'Kshitish Deshpande',
+        'img': 'images/ksdfg.jpg',
         'title': 'Lead Developer',
         'desc': `The shy, yet overconfident otaku of S.C.R.I.P.T - Kshitish coordinates
         the team in our
@@ -53,6 +57,7 @@ const members = [
     },
     {
         'name': 'Akhil Narang',
+        'img': 'images/akhil.PNG',
         'title': 'Lead Developer',
         'desc': `Pizza lover, and GitHub pusher - Akhil is our Hulk in size and The Flash
         in coding!`,
@@ -65,6 +70,7 @@ const members = [
     },
     {
         'name': 'Pranav Bakre',
+        'img': 'images/pranav.PNG',
         'title': 'Lead Developer',
         'desc': `Pizza lover, and GitHub pusher - Akhil is our Hulk in size and The Flash
         in coding!`,
@@ -77,6 +83,7 @@ const members = [
     },
     {
         'name': 'Naitik Parmar',
+        'img': 'images/naitik.jpg',
         'title': 'Lead Web Developer',
         'desc': `A coding enthusiast, Naitik tries to solve real life problems using
         programming because for
@@ -87,5 +94,77 @@ const members = [
         'link2': 'https://www.instagram.com/naitik0909',
         'link3-class': 'icon-github',
         'link3': 'https://www.github.com/Naitik0909'
+    },
+    {
+        'name': 'Alok Bhawankar',
+        'img': 'images/alok.jpg',
+        'title': 'Joint Treasurer',
+        'desc': `Anime freak and hacker, with a pretty much non-functional laptop - Alok
+        is our Kali Linux
+        sensei. From managing funds to keeping track of our expenses, he can account for every rupee
+        spent!`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/alok-bhawankar-122887148/',
+        'link2-class': 'fab fa-instagram',
+        'link2': 'https://www.instagram.com/the_alpha29/',
+        'link3-class': 'icon-github',
+        'link3': 'https://github.com/alrocks29'
+    },
+    {
+        'name': 'Kashyap Kale',
+        'img': 'images/kashyap.JPG',
+        'title': 'Person of Contact',
+        'desc': `Beware, he may come for your password! A cyber security enthusiast,
+        camera supplier and PR
+        guy, Kashyap is the reason people receive our email and messages.`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/kashyap-kale-a71398151',
+        'link2-class': 'fab fa-instagram',
+        'link2': 'https://www.instagram.com/kashyapkale',
+        'link3-class': 'icon-twitter',
+        'link3': 'https://www.twitter.com/Meetkashyapkale'
+    },
+    {
+        'name': 'Atharva Jibhakate',
+        'img': 'images/atharva.jpg',
+        'title': 'Social Media Manager',
+        'desc': `Mostly interested in Social Media handling and Presentation part, Atharva
+        likes to work with
+        the us for the goodness of the club, and the students we intend to help.`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/atharva-jibhakate-6414b9176',
+        'link2-class': 'fab fa-instagram',
+        'link2': 'https://www.instagram.com/i_am_atharva18/',
+        'link3-class': 'icon-facebook',
+        'link3': 'https://m.facebook.com/atharva.jibhakate'
+    },
+    {
+        'name': 'Deeva Modi',
+        'img': 'images/deeva.jpg',
+        'title': 'Graphic Designer',
+        'desc': `Deeva is good at creativity, designing, drawing, and what not! Also if
+        you need any "Yoga"
+        related tips .. she's there!`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/deeva-modi-99306117a',
+        'link2-class': 'fab fa-instagram',
+        'link2': 'https://www.instagram.com/_modiji__/',
+        'link3-class': 'icon-twitter',
+        'link3': 'https://twitter.com/yoga_girl25?s=09'
+    },
+    {
+        'name': 'Abhishek Misar',
+        'img': 'images/abhishek.jpg',
+        'title': 'Graphic Designer',
+        'desc': `Somewhere in a state of trance, Abhishek is mostly into motion graphics
+        and graphic design.
+        He is also an avid gamer and also a coding fanatic. If you are looking for him, you will
+        mostly find him in Windhelm.`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/abhishek-misar-7689a9192',
+        'link2-class': 'fab fa-instagram',
+        'link2': 'https://instagram.com/abhishek_0413?igshid=101mkc6w1nv3e',
+        'link3-class': 'fab fa-youtube',
+        'link3': 'https://www.youtube.com/channel/UCxqfIy1jxitpOIiJQDXG39w'
     }
 ]
