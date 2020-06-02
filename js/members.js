73f4ee40ae1506dce77686e9cd680bef54e71d05
Aniket Raj <aniketronaldo10@gmail.com>
2020-06-02 10:11:35 +0530

    JS object creation initiated for members, Bakre helping now

---
+++ js/members.js
@@ -0,0 +1,91 @@
+const members = [
+    {
+        'name': 'Aniket Raj',
+        'title': 'President',
+        'desc': `Winner of Ready Set Code '18, and our primary Techo Lecturer, Aniket
+        brings out the best of the team with his wide-ranging skills, but is still immature with
+        his love for Spider-Man.`,
+        'link1-class': 'fab fa-instagram',
+        'link1': 'https://www.instagram.com/aniket_spidey',
+        'link2-class': 'fas fa-globe-asia',
+        'link2': 'http://www.aniketraj.co/',
+        'link3-class': 'icon-github',
+        'link3': 'https://www.github.com/aniket-spidey'
+    },
+    {
+        'name': 'Ritom Gupta',
+        'title': 'Chairman & Co-founder',
+        'desc': `The crazy, yet sensible founder of S.C.R.I.P.T, Ritom manages the team in
+        all activities, and
+        is the guy behind our administration, designs, and website updates.`,
+        'link1-class': 'fab fa-instagram',
+        'link1': 'https://www.instagram.com/monsieur_rittman/',
+        'link2-class': 'fas fa-globe-asia',
+        'link2': 'https://rightonrittman.in/',
+        'link3-class': 'icon-github',
+        'link3': 'https://www.github.com/rittmang'
+    },
+    {
+        'name': 'Vignesh Iyer',
+        'title': 'Co-Founder & Joint Treasurer',
+        'desc': `The silent and dependable founder of S.C.R.I.P.T, Vignesh is our finance
+        head and chief
+        advisor. That he spends time on Quora, should suffice for this caption.`,
+        'link1-class': 'fab fa-instagram',
+        'link1': 'https://www.instagram.com/the_viggyboy/',
+        'link2-class': 'icon-twitter',
+        'link2': 'https://twitter.com/logical_vhi?s=08',
+        'link3-class': 'icon-github',
+        'link3': 'https://github.com/vigu184'
+    },
+    {
+        'name': 'Kshitish Deshpande',
+        'title': 'Lead Developer',
+        'desc': `The shy, yet overconfident otaku of S.C.R.I.P.T - Kshitish coordinates
+        the team in our
+        events, and is an anime fan! Weirdly obsessed with cats though…`,
+        'link1-class': 'fab fa-reddit',
+        'link1': 'https://www.reddit.com/user/rc1247',
+        'link2-class': 'fab fa-facebook',
+        'link2': 'https://www.facebook.com/KSD.RULES',
+        'link3-class': 'icon-github',
+        'link3': 'https://github.com/ksdfg'
+    },
+    {
+        'name': 'Akhil Narang',
+        'title': 'Lead Developer',
+        'desc': `Pizza lover, and GitHub pusher - Akhil is our Hulk in size and The Flash
+        in coding!`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/akhilnarang',
+        'link2-class': 'fas fa-globe-asia',
+        'link2': 'https://akhilnarang.dev',
+        'link3-class': 'icon-github',
+        'link3': 'https://github.com/akhilnarang'
+    },
+    {
+        'name': 'Pranav Bakre',
+        'title': 'Lead Developer',
+        'desc': `Pizza lover, and GitHub pusher - Akhil is our Hulk in size and The Flash
+        in coding!`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/akhilnarang',
+        'link2-class': 'fas fa-globe-asia',
+        'link2': 'https://akhilnarang.dev',
+        'link3-class': 'icon-github',
+        'link3': 'https://github.com/akhilnarang'
+    },
+    {
+        'name': 'Naitik Parmar',
+        'title': 'Lead Web Developer',
+        'desc': `A coding enthusiast, Naitik tries to solve real life problems using
+        programming because for
+        every problem, there's always a logic to the solution.`,
+        'link1-class': 'fab fa-linkedin',
+        'link1': 'https://www.linkedin.com/in/naitik-parmar-174a2b183',
+        'link2-class': 'fab fa-instagram',
+        'link2': 'https://www.instagram.com/naitik0909',
+        'link3-class': 'icon-github',
+        'link3': 'https://www.github.com/Naitik0909'
+    }
+]
